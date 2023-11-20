7b3adf86ac3bb1d50967f2db68175fd23dd8f9c2
Camilojavier <camilojavier3@gmail.com>
2023-11-20 15:03:15 -0400

    code cleanup

--- src/main/java/com/diplomado/tarea/repositories/UserRoleRepository.java
+++ src/main/java/com/diplomado/tarea/repositories/UserRoleRepository.java
@@ -4,7 +4,6 @@ import com.diplomado.tarea.domain.entities.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
     List<UserRole> findAllByRoles_Id(Integer roleId);
